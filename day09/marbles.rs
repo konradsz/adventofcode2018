d4f82b6ddbdb6504d9353cc15d76bdf90eeb5000
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2018-12-09 19:27:50 +0100

    reduce variable scope

--- day09/marbles.rs
+++ day09/marbles.rs
@@ -6,14 +6,13 @@ fn calculate_score(number_of_players: usize, max_marble_value: usize) -> usize {
     let mut players_score = vec![0; number_of_players];
 
     for marble in 1..=max_marble_value {
-        let current_player = marble % number_of_players;
-
         if marble % 23 == 0 {
             for _ in 0..7 {
                 let m = marbles.pop_back().unwrap();
                 marbles.push_front(m);
             }
             let score = marbles.pop_front().unwrap();
+            let current_player = marble % number_of_players;
             players_score[current_player] += marble;
             players_score[current_player] += score;
         } else {
